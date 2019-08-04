88a82d289a146715bfd2ed331b744bcfdc25a763
MartinChh <30865924+MartinChh@users.noreply.github.com>
2019-08-04 02:42:17 +0200

    Adjusted form design

--- TextFileStatisticProcessor/Form1.Designer.cs
+++ TextFileStatisticProcessor/Form1.Designer.cs
@@ -94,7 +94,7 @@
             // 
             this.progressBar1.Location = new System.Drawing.Point(61, 167);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(342, 13);
+            this.progressBar1.Size = new System.Drawing.Size(397, 13);
             this.progressBar1.TabIndex = 4;
             // 
             // sentenceCountText
